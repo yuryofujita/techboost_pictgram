9dbbb715878b82106791ad0bca01897925e5fc3b
yuryofujita <dasman74@hotmail.com>
2020-08-22 10:21:01 +0000

    Rails 04 User新規登録に潜む問題を解決していく

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'jquery-rails'
 gem 'bootstrap-sass'
+gem 'bcrypt'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_134748) do
+ActiveRecord::Schema.define(version: 2020_08_22_101440) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
