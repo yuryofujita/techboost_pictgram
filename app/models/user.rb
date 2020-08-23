f976781f2f53689f2e831d7e2dae4b054f2adcd8
yuryofujita <dasman74@hotmail.com>
2020-08-23 02:30:41 +0000

    [課題]Rails 04 User新規登録に潜む問題を解決していく

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_REGEX }
+  PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
+  validates :password, presence: true, format: { with: PW_REGEX }
   
   has_secure_password
 end
